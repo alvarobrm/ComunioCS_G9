7b9a8cf4ec212d68227e2af9a1abcfb646680380
palv94 <p.alvarezga@alumnos.urjc.es>
2016-02-26 16:42:37 +0100

    (Feature - Andres): Añado archivos .java y .rb

---
+++ feature/andres.rb
@@ -0,0 +1,16 @@
+Given /^(.+) in (.+) with (.+) in the league market$/ do |user, league, player|
+    # do something
+end
+
+When /^market button is pressed$/ do 
+    # do something
+end
+
+Then /^(.+) must be shown in the List$/ do |player|
+    # do something
+end
+
+And /^(.+) price must be (.+)$/ do |player, cost|
+    # do something
+end
+
