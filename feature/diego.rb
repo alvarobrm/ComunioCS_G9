7c8e8446707f254242255e1c0932731aabe4bfca
palv94 <p.alvarezga@alumnos.urjc.es>
2016-02-26 16:42:49 +0100

    (Feature - Diego): Añado archivos .java y .rb

---
+++ feature/diego.rb
@@ -0,0 +1,75 @@
+Given /^there is a goalkeeper in my team$/ do 
+    # do something
+end
+
+Given /^I have at least one defender in my first team$/ do 
+    # do something
+end
+
+Given /^I have at least one midfield player in my team$/ do 
+    # do something
+end
+
+Given /^I have at least one forward in my team$/ do 
+    # do something
+end
+
+Given /^I have a void position in my first team$/ do 
+    # do something
+end
+
+When /^I click on the goalkeeper in my team$/ do 
+    # do something
+end
+
+When /^I click on one of the defenders in my team$/ do 
+    # do something
+end
+
+When /^I click on one of the midfield players in my team$/ do 
+    # do something
+end
+
+When /^I click on one of the forwards in my team$/ do 
+    # do something
+end
+
+When /^I click on the void position$/ do 
+    # do something
+end
+
+Then /^the goalkeeper is removed from the first team$/ do 
+    # do something
+end
+
+Then /^that defender is removed from the first team$/ do 
+    # do something
+end
+
+Then /^that midfield player is removed from the first team$/ do 
+    # do something
+end
+
+Then /^that forward is removed from the first team$/ do 
+    # do something
+end
+
+Then /^the app should show me an error message$/ do 
+    # do something
+end
+
+And /^the goalkeeper stays in the bench$/ do 
+    # do something
+end
+
+And /^the defender stays in the bench$/ do 
+    # do something
+end
+
+And /^the midfield player stays in the bench$/ do 
+    # do something
+end
+
+And /^the forward stays in the bench$/ do 
+    # do something
+end
