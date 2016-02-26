b572f92c07bc9c2906305e53cf7c1900e6b0de26
palv94 <p.alvarezga@alumnos.urjc.es>
2016-02-26 16:43:15 +0100

    (Feature - Oscar): Añado archivos .java y .rb

---
+++ feature/oscar.rb
@@ -0,0 +1,35 @@
+Given /^User press the \"([^\"]*)\" button$/ do |menu|
+    # do something
+end
+
+When /^the new page loads$/ do 
+    # do something
+end
+
+Then /^he should see an alert to join a league$/ do 
+    # do something
+end
+
+Then /^he should see his username and points$/ do 
+    # do something
+end
+
+Then /^he should see an alert to wait for the season's start$/ do 
+    # do something
+end
+
+Then /^he should see his username, points and final rank on his league$/ do 
+    # do something
+end
+
+Then /^he should redirected to the loging page$/ do 
+    # do something
+end
+
+And /^he is not on a league$/ do 
+    # do something
+end
+
+And /^he is on a private league$/ do 
+    # do something
+end
